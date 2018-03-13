9754f5e23e69281cb1c8fccff7526674c6c2adc9
Armin Kuhestani <armin.kuhestani@gmail.com>
2018-03-13 14:16:05 +1100

    revamp after new wireframe

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
-$(document).ready(function () {
-  $(".navbar-nav li a").click(function(event) {
-    $(".navbar-collapse").collapse('hide');
-  });
-});
-
-// $(document).on('click','.navbar-collapse.in',function(e) {
-//     if( $(e.target).is('a:not(".dropdown-toggle")') ) {
-//         $(this).collapse('hide');
-//     }
+// $(document).ready(function () {
+//   $(".navbar-nav li a").click(function(event) {
+//     $(".navbar-collapse").collapse('hide');
+//   });
 // });
