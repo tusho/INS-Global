ab6eec2ca9586e5a02144a5bf52aab37ab98efbd
Armin Kuhestani <armin.kuhestani@gmail.com>
2018-03-16 09:55:26 +1100

    changing dimensions to fit guidelines

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 //     $(".navbar-collapse").collapse('hide');
 //   });
 // });
+
+
+//
+// $('#stuff').click(function(){
+//    $('#stuff').animate({ 'margin-left': '+=150px'}, 1000);
+//    return false;
+// });
+
+
+// $(document).ready(function(){
+//        $('#stuff').click(function(){
+//            $('body').css("margin-left",200);
+//        });
+//        $('#stuff').click(function(){
+//            $('body').css("margin-left",0);
+//        });
+//    });
