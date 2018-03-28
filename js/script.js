4f89800e639e38b1e9e255707dda3f0cdc9fcbd4
Armin Kuhestani <armin.kuhestani@gmail.com>
2018-03-28 17:06:31 +1100

    futher form styling

--- js/script.js
+++ js/script.js
@@ -89,21 +89,29 @@ $(function () {
 $('#form_name').focus(function(){
 $(this).css('backgroundColor','#2b2a29');
 $(this).css('color', 'white');
+$(this).css('fontSize', '0.8em');
+$(this).css('textTransform', 'none');
 });
 
 $('#form_email').focus(function(){
 $(this).css('backgroundColor','#2b2a29');
 $(this).css('color', 'white');
+$(this).css('fontSize', '0.7em');
+$(this).css('textTransform', 'none');
 });
 
 $('#form_phone').focus(function(){
 $(this).css('backgroundColor','#2b2a29');
 $(this).css('color', 'white');
+$(this).css('fontSize', '0.7em');
+$(this).css('textTransform', 'none');
 });
 
 $('#form_message').focus(function(){
 $(this).css('backgroundColor','#2b2a29');
 $(this).css('color', 'white');
+$(this).css('fontSize', '0.7em');
+$(this).css('textTransform', 'none');
 });
 
 // $('#myInput').blur(function(){
