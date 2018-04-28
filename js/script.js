d24dc7ab7a0c4fc513ea202a91af6d1d98097c43
Armin Kuhestani <armin.kuhestani@gmail.com>
2018-04-28 16:00:39 +1000

    added photo gallery

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
-// $(document).ready(function () {
-//   $(".navbar-nav li a").click(function(event) {
-//     $(".navbar-collapse").collapse('hide');
-//   });
-// });
-
-
-//
-// $('#stuff').click(function(){
-//    $('#stuff').animate({ 'margin-left': '+=150px'}, 1000);
-//    return false;
-// });
-
-
-// $(document).ready(function(){
-//        $('#stuff').click(function(){
-//            $('body').css("margin-left",200);
-//        });
-//        $('#stuff').click(function(){
-//            $('body').css("margin-left",0);
-//        });
-//    });
 var open = false;
 
 $("body").click(function() {
